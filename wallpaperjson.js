8074d4f62cc65740be76d1f62c84457cb84c12c8
Hiten Soni <soni_hiten@yahoo.com>
2016-08-03 23:19:14 +0530

    json
    json update

--- wallpaperjson.js
+++ wallpaperjson.js
@@ -14,5 +14,128 @@
     {
     "name" : "Random", 
     "link" : "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Random.png"
-    }
+    },
+    {
+    "name" : "Ace", 
+    "link" : "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Ace.png"
+    },
+    {
+    "name" : "Arena", 
+    "link" : "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Arena.png"
+    },
+    {
+    "name" : "Arena1", 
+    "link" : "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Arena1.png"
+    },
+    {
+    "name" : "Ascend", 
+    "link" : "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Ascend.png"
+    },
+    {
+    "name" : "Auric1", 
+    "link" : "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Auric1.png"
+    },
+    {
+    "name" : "Blade", 
+    "link" : "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Blade.png"
+    },
+    {
+    "name" : "Blak", 
+    "link" : "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Blak.png"
+    },	
+    {
+    "name" : "Blush", 
+    "link" : "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Blush.png"
+    },
+    {
+    "name" : "Bora", 
+    "link" : "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Bora.png"
+    },
+    {
+    "name" : "Britzer", 
+    "link" : "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Britzer.png"
+    },
+    {
+    "name" : "Britzer1", 
+    "link" : "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Britzer1.png"
+    },
+    { 
+    "name" : "CardUi", 
+    "link" : "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/CardUi.png"
+    },
+    {
+    "name" : "Cherry", 
+    "link" : "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Cherry.png"
+    },
+    {
+    "name" : "Dare", 
+    "link" : "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Dare.png"
+    },
+    {
+    "name" : "Dare1", 
+    "link" : "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Dare1.png"
+    },
+    {
+    "name" : "Dazz", 
+    "link" : "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Dazz.png"
+    },
+    {
+    "name" : "Eco", 
+    "link" : "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Eco.png"
+    },
+    {
+    "name" : "Edge", 
+    "link" : "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Edge.png"
+    },
+    {
+    "name" : "Edge1", 
+    "link" : "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Edge1.png"
+    },
+    {
+    "name" : "Edge2", 
+    "link" : "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Edge2.png"
+    },
+    {
+    "name" : "Eminence", 
+    "link" : "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Eminence.png"
+    },
 ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
