019e9228b4d7051212cb66138e83511777a39f77
Hiten Soni <soni_hiten@yahoo.com>
2016-08-03 16:20:44 +0530

    c3
    c3

--- wallpaperjson.js
+++ wallpaperjson.js
@@ -1,18 +1,18 @@
 [
     {
-    "name" : "Blak Ui", 
-    "link" : "http://www.bigdx.com/userwalls/ideadesigns/WallsHd/Blak_wall.png"
+    "name" : "Bold", 
+    "link" : "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Bold.jpg"
     },
     {
-    "name" : "Rise Ui", 
-    "link" : "http://www.bigdx.com/userwalls/ideadesigns/WallsHd/rise_ui.png"
+    "name" : "Inspire", 
+    "link" : "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Inspire.png"
     },
     {
-    "name" : "Card Ui", 
-    "link" : "http://www.bigdx.com/userwalls/ideadesigns/WallsHd/Cherry.png"
+    "name" : "Prime", 
+    "link" : "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Prime.png"
     },
     {
-    "name" : "Dazz", 
-    "link" : "http://www.bigdx.com/userwalls/ideadesigns/WallsHd/Dazz.png"
+    "name" : "Random", 
+    "link" : "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Random.png"
     }
 ]
