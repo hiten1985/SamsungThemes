efb94893d59bad4a1456f997b4476d524b852a6a
Hiten Soni <soni_hiten@yahoo.com>
2016-08-03 15:34:25 +0530

    c2
    c2

---
+++ wallpaperjson.js
@@ -0,0 +1,18 @@
+[
+    {
+    "name" : "Blak Ui", 
+    "link" : "http://www.bigdx.com/userwalls/ideadesigns/WallsHd/Blak_wall.png"
+    },
+    {
+    "name" : "Rise Ui", 
+    "link" : "http://www.bigdx.com/userwalls/ideadesigns/WallsHd/rise_ui.png"
+    },
+    {
+    "name" : "Card Ui", 
+    "link" : "http://www.bigdx.com/userwalls/ideadesigns/WallsHd/Cherry.png"
+    },
+    {
+    "name" : "Dazz", 
+    "link" : "http://www.bigdx.com/userwalls/ideadesigns/WallsHd/Dazz.png"
+    }
+]
