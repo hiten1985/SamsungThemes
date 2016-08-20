dd215f6460b9e86194cf779d11e2cf266b6b75c3
Hiten Soni <soni_hiten@yahoo.com>
2016-08-20 16:01:52 +0530

    s3
    s3

--- Wallpapers.js
+++ Wallpapers.js
@@ -6,40 +6,11 @@
 		"name": "Inspire1",
 		"link": "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Inspire.png",
 		"thumb": "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Inspire.png"
-	},
-      {
-		"name": "Inspire",
-		"link": "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Inspire.png",
-		"thumb": "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Inspire.png"
-	}, 
-	{
-		"name": "Fusion",
-		"link": "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Fusion.png"
-	}, 
-	{
-		"name": "Card Ui",
-		"link": "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/CardUi.png"
-	}, 
-	{
-		"name": "Dazz",
-		"link": "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Dazz.png"
-	}, 
-	{
-		"name": "Ascend",
-		"link": "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Ascend.png"
-	}, 
-        {
-		"name": "Britzer1",
-		"link": "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Britzer1.jpg"
-	}, 
-	{
-		"name": "Prime",
-		"link": "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Prime.png"
 	}
     ]
   },
   {
-    "category_name":"Bokeh",
+    "category_name":"Pattern & Stripes",
     "walls":[
       {
         "name":"Auric",
@@ -48,12 +19,21 @@
     ]
   },
   {
-    "category_name":"Patterns & Stripes",
+    "category_name":"Bokeh",
     "walls":[
       {
         "name":"Eminence",
         "link":"https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Eminence.png"
+      },
+{
+    "category_name":"Misc",
+    "walls":[
+      {
+        "name":"Eminence",
+        "link":"https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Eminence.png"
+	  }
+         ]
+        }
+       ]
       }
-    ]
-  }
-]
+     ]
