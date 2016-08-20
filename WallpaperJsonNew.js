74fdb37912584d57c8c177c7b2d7fa5e3b6de269
Hiten Soni <soni_hiten@yahoo.com>
2016-08-20 12:36:03 +0530

    7
    7

--- WallpaperJsonNew.js
+++ WallpaperJsonNew.js
@@ -3,35 +3,48 @@
 	"walls": [{
 		"name": "Inspire",
 		"link": "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Inspire.png"
-	}, {
+	}, 
+	{
+		"name": "Fusion",
+		"link": "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Fusion.png"
+	}, 
+	{
 		"name": "Card Ui",
 		"link": "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/CardUi.png"
-	}, {
+	}, 
+	{
 		"name": "Dazz",
 		"link": "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Dazz.png"
-	}, {
+	}, 
+	{
 		"name": "Prime",
 		"link": "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Prime.png"
 	}
 	]
-	}, {
+	}, 
+	{
 	"category_name": "Baloon",
 	"walls": [{
 		"name": "Cherry",
 		"link": "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Cherry.png"
-	}, {
+	}, 
+	{
 		"name": "Dare",
 		"link": "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Dare.png"
-	}, {
+	},
+	{
 		"name": "Dare1",
 		"link": "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Dare1.png"
-	}, {
+	}, 
+	{
 		"name": "Dazz",
 		"link": "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Dazz.png"
-	}, {
+	}, 
+	{
 		"name": "Eco",
 		"link": "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Eco.png"
-	}, {
+	}, 
+	{
 		"name": "Edge",
 		"link": "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Edge.png"
 	}, {
