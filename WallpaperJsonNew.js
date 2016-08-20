5e8eb57535ce218319f1ab64be6b0b99d0e3c2ba
Hiten Soni <soni_hiten@yahoo.com>
2016-08-20 13:10:03 +0530

    10
    10

--- WallpaperJsonNew.js
+++ WallpaperJsonNew.js
@@ -21,78 +21,8 @@
 		"link": "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Ascend.png"
 	}, 
 	{
-		"name": "Britzer1",
-		"link": "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Britzer1.jpg"
-	}, 
-	{
 		"name": "Prime",
 		"link": "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Prime.png"
 	}
-	]
-	}, 
-	{
-	"category_name": "Baloon",
-	"walls": [{
-		"name": "Cherry",
-		"link": "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Cherry.png"
-	}, 
-	{
-		"name": "Dare",
-		"link": "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Dare.png"
-	},
-	{
-		"name": "Dare1",
-		"link": "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Dare1.png"
-	}, 
-	{
-		"name": "Dazz",
-		"link": "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Dazz.png"
-	}, 
-	{
-		"name": "Eco",
-		"link": "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Eco.png"
-	}, 
-	{
-		"name": "Edge",
-		"link": "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Edge.png"
-	}, {
-		"name": "Edge1",
-		"link": "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Edge1.png"
-	}, {
-		"name": "Edge2",
-		"link": "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Edge2.png"
-	}, {
-		"name": "Eminence",
-		"link": "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Eminence.png"
-	}]
-	}, {
-	"category_name": "Firework",
-	"walls": [{
-		"name": "Cherry",
-		"link": "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Cherry.png"
-	}, {
-		"name": "Dare",
-		"link": "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Dare.png"
-	}, {
-		"name": "Dare1",
-		"link": "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Dare1.png"
-	}, {
-		"name": "Dazz",
-		"link": "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Dazz.png"
-	}, {
-		"name": "Eco",
-		"link": "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Eco.png"
-	}, {
-		"name": "Edge",
-		"link": "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Edge.png"
-	}, {
-		"name": "Edge1",
-		"link": "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Edge1.png"
-	}, {
-		"name": "Edge2",
-		"link": "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Edge2.png"
-	}, {
-		"name": "Eminence",
-		"link": "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Eminence.png"
-	}]
+	 ]
 }]
